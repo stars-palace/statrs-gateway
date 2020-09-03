06fd27e273d1abc3b45a38f9469b41e7727c0db3
tangx <tangxka@yonyou.com>
2020-09-03 23:27:32 +0800

    项目初始化

---
+++ gateway.go
@@ -0,0 +1,11 @@
+package statrs_gateway
+
+/**
+ * Copyright (C) @2020 hugo network Co. Ltd
+ * 网关
+ * @author: hugo
+ * @version: 1.0
+ * @date: 2020/9/3
+ * @time: 23:25
+ * @description:
+ */
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/stars-palace/statrs-gateway
+
+go 1.14
+
+require github.com/stars-palace/statrs-config v1.0.5 // indirect
